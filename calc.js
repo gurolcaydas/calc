03eac968cbd5b65b0aff49b2bee002681cb4deb1
Gurol <gurol@caydas.com>
2022-11-28 19:58:35 +0300

    Yeter la!

--- calc.js
+++ calc.js
@@ -1,52 +1,47 @@
 var answer;
 var lastCalc;
 var toCalculate;
-var helpText = "\n \n ------- \nF1 - Help! \n F2 - Last Calculation \n F4 - Last Answer \n F12 - Clear Screen \n";
+var helpText =
+  "\n \n ------- \nF1 - Help! \n F2 - Last Calculation \n F4 - Last Answer \n F12 - Clear Screen \n";
 
-const node = document.getElementById("calcField");
-document.getElementById("calcTape").innerHTML = helpText;
-node.addEventListener("keyup", function (event) {
+const nodeInput = document.getElementById("calcField");
+const nodeTape = document.getElementById("calcTape");
+
+nodeTape.innerHTML = helpText;
+nodeInput.addEventListener("keyup", function (event) {
   switch (event.key) {
-    case "Enter":  // pressing enter does the job
-      lastCalc = document.getElementById("calcField").value;
+    case "Enter": // pressing enter does the job
+      lastCalc = nodeInput.value;
       toCalculate = lastCalc;
       toCalculate = toCalculate.replace("^", "**");
       toCalculate = toCalculate.replace("sin(", "Math.sin((Math.PI/180)*");
       toCalculate = toCalculate.replace("cos(", "Math.cos((Math.PI/180)*");
       toCalculate = toCalculate.replace("pi", "Math.PI");
       try {
-      answer = eval(toCalculate);
-      document.getElementById("calcTape").innerHTML =
-        document.getElementById("calcField").value +
-        " = " +
-        answer +
-        "\n" +
-        document.getElementById("calcTape").innerHTML;
+        answer = eval(toCalculate);
+        nodeTape.innerHTML =
+          nodeInput.value + " = " + answer + "\n" + nodeTape.innerHTML;
 
-      document.getElementById("calcField").value = "";      
+        nodeInput.value = "";
       } catch (error) {
-        document.getElementById("calcTape").innerHTML =
-        document.getElementById("calcField").value+
-        "\n"+ document.getElementById("calcTape").innerHTML ;
-        
-      } 
+        nodeTape.innerHTML = nodeInput.value + "\n" + nodeTape.innerHTML;
+      }
       break;
     case "F1": // help
-      document.getElementById("calcTape").innerHTML = helpText + "\n ------- \n" + document.getElementById("calcTape").innerHTML;
-      document.getElementById("calcField").value = "";
+      nodeTape.innerHTML = helpText + "\n ------- \n" + nodeTape.innerHTML;
+      nodeInput.value = "";
       break;
     case "F2": // callback last calculation
-      document.getElementById("calcField").value = lastCalc;
-      break;      
+      nodeInput.value = lastCalc;
+      break;
     case "F4": // return answer
-      document.getElementById("calcField").value =
-        document.getElementById("calcField").value.replace("F4", "") + answer;
+      nodeInput.value = answer;
       break;
     case "F12": // clear all
-      document.getElementById("calcTape").innerHTML = "";
-      document.getElementById("calcField").value = "";
+      nodeTape.innerHTML = "";
+      nodeInput.value = "";
       break;
     default:
       break;
-  } 
+  }
 });
