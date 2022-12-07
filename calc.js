fffd72d321ce71f7153ae79f3c906195ed806af2
Gurol <gurol@caydas.com>
2022-12-07 12:16:47 +0300

    8

--- calc.js
+++ calc.js
@@ -2,17 +2,17 @@ var answer;
 var lastCalc;
 var toCalculate;
 var helpText =
-  "\n\n ------- \n F1 - Help! \n F2 - Last Calculation \n F4 - Last Answer \n F10 - Clear Screen \n" +
-  "------- \n- * + / ^ \n sin() cos() pi \n" +
-  "------- \n& - AND \n | - OR \n\n";
+  "<br><br> ------- <br> F1 - Help! <br> F2 - Last Calculation <br> F3 - Memory <br> F4 - Last Answer <br> F10 - Clear Screen <br>" +
+  "------- <br>- * + / ^ <br> sin() cos() pi <br>" +
+  "------- <br>& - AND <br> | - OR <br><br>";
 
 const nodeInput = document.getElementById("calcField");
-const nodeTape = document.getElementById("calcTape");
+const nodeMemory = document.getElementById("calcMem");
 
 // document.getElementById("calcDiv").style.height = screen.availHeight + "px";
 // document.getElementById("calcDiv").style.width = screen.availWidth + "px";
 // version 0.1 - 6 Dec 2022
-nodeTape.innerHTML = helpText;
+nodeMemory.innerHTML = helpText;
 nodeInput.addEventListener("keyup", function (event) {
   switch (event.key) {
     case "Enter": // pressing enter does the job
@@ -24,24 +24,33 @@ nodeInput.addEventListener("keyup", function (event) {
       toCalculate = toCalculate.replace("pi", "Math.PI");
       try {
         answer = eval(toCalculate);
-        nodeTape.innerHTML =
-          nodeInput.value + " = " + answer + "\n" + nodeTape.innerHTML;
+        nodeMemory.innerHTML =
+          nodeInput.value +
+          " = " +
+          answer +
+          "<span class='copy material-icons' onclick='geriOku2(" +
+          answer +
+          ");'>copy_all</span><br>" +
+          nodeMemory.innerHTML;
         nodeInput.value = "";
       } catch (error) {
-        nodeTape.innerHTML = nodeInput.value + "\n" + nodeTape.innerHTML;
+        nodeMemory.innerHTML = nodeInput.value + "<br>" + nodeMemory.innerHTML;
       }
       break;
     case "F1": // help
-      nodeTape.innerHTML = helpText + nodeTape.innerHTML;
+      nodeMemory.innerHTML = helpText + nodeMemory.innerHTML;
       break;
     case "F2": // callback last calculation
       nodeInput.value = lastCalc;
+      break;
+    case "ArrowUp": // memory
+      nodeInput.value = nodeInput.value + answer;
       break;
     case "F4": // return answer
       nodeInput.value = nodeInput.value + answer;
       break;
     case "F10": // clear all
-      nodeTape.innerHTML = "";
+      nodeMemory.innerHTML = "";
       nodeInput.value = "";
       break;
     default:
@@ -56,4 +65,7 @@ function popUp(a) {
 function geriOku(a) {
   document.getElementById("calcField").value =
     document.getElementById(a).innerHTML;
+}
+function geriOku2(a) {
+  document.getElementById("calcField").value = a;
 }
