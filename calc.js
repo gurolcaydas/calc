748c8c4cbaf5534c11c795d44a3f2bf6c757147f
Gurol <gurol@caydas.com>
2022-12-07 16:06:41 +0300

    9

--- calc.js
+++ calc.js
@@ -2,7 +2,8 @@ var answer;
 var lastCalc;
 var toCalculate;
 var helpText =
-  "<br><br> ------- <br> F1 - Help! <br> F2 - Last Calculation <br> F3 - Memory <br> F4 - Last Answer <br> F10 - Clear Screen <br>" +
+  "<br><br> ------- <br> F1 - Help! <br> Up - Last Calculation <br> Down - Last Answer <br> F10 - Clear Screen <br>" +
+  "Any key - Focus to Input <br>" +
   "------- <br>- * + / ^ <br> sin() cos() pi <br>" +
   "------- <br>& - AND <br> | - OR <br><br>";
 
@@ -13,40 +14,46 @@ const nodeMemory = document.getElementById("calcMem");
 // document.getElementById("calcDiv").style.width = screen.availWidth + "px";
 // version 0.1 - 6 Dec 2022
 nodeMemory.innerHTML = helpText;
-nodeInput.addEventListener("keyup", function (event) {
+window.addEventListener("keyup", function (event) {
+  if (nodeInput.focus() == false) {
+    nodeInput.focus();
+  }
+
   switch (event.key) {
     case "Enter": // pressing enter does the job
       lastCalc = nodeInput.value;
-      toCalculate = lastCalc;
-      toCalculate = toCalculate.replace("^", "**");
-      toCalculate = toCalculate.replace("sin(", "Math.sin((Math.PI/180)*");
-      toCalculate = toCalculate.replace("cos(", "Math.cos((Math.PI/180)*");
-      toCalculate = toCalculate.replace("pi", "Math.PI");
-      try {
-        answer = eval(toCalculate);
-        nodeMemory.innerHTML =
-          nodeInput.value +
-          " = " +
-          answer +
-          "<span class='copy material-icons' onclick='geriOku2(" +
-          answer +
-          ");'>copy_all</span><br>" +
-          nodeMemory.innerHTML;
+      if (lastCalc !== "") {
+        toCalculate = lastCalc;
+        toCalculate = toCalculate.replace("^", "**");
+        toCalculate = toCalculate.replace("sin(", "Math.sin((Math.PI/180)*");
+        toCalculate = toCalculate.replace("cos(", "Math.cos((Math.PI/180)*");
+        toCalculate = toCalculate.replace("pi", "Math.PI");
+        try {
+          answer = eval(toCalculate);
+          nodeMemory.innerHTML =
+            nodeInput.value +
+            " = " +
+            answer +
+            "<span class='copy material-icons' style='margin-left:20px;' onclick='geriOku2(" +
+            answer +
+            ");'>copy_all</span><br>" +
+            nodeMemory.innerHTML;
+        } catch (error) {
+          nodeMemory.innerHTML =
+            nodeInput.value + "<br>" + nodeMemory.innerHTML;
+        }
         nodeInput.value = "";
-      } catch (error) {
-        nodeMemory.innerHTML = nodeInput.value + "<br>" + nodeMemory.innerHTML;
       }
       break;
     case "F1": // help
       nodeMemory.innerHTML = helpText + nodeMemory.innerHTML;
       break;
-    case "F2": // callback last calculation
+    case "Up": // callback last calculation
+    case "ArrowUp": // callback last calculation
       nodeInput.value = lastCalc;
       break;
-    case "ArrowUp": // memory
-      nodeInput.value = nodeInput.value + answer;
-      break;
-    case "F4": // return answer
+    case "Down": //  return answer
+    case "ArrowDown": //  return answer
       nodeInput.value = nodeInput.value + answer;
       break;
     case "F10": // clear all
@@ -58,14 +65,8 @@ nodeInput.addEventListener("keyup", function (event) {
   }
 });
 
-function popUp(a) {
-  document.getElementById(a).innerHTML = answer;
-  // document.querySelector("input").click();
-}
-function geriOku(a) {
-  document.getElementById("calcField").value =
-    document.getElementById(a).innerHTML;
-}
+// document.querySelector("input").click();
+
 function geriOku2(a) {
   document.getElementById("calcField").value = a;
 }
